30ca2aa96f1a4969e9262c491504557519b41b8f
AlexeyShakov <alexei_96@inbox.ru>
2022-07-29 01:12:10 +0300

    added aggregation notes

--- movie_app/views.py
+++ movie_app/views.py
@@ -1,12 +1,15 @@
 from django.shortcuts import render, get_object_or_404
+from django.db.models import F, Sum, Max, Min, Count, Avg
 from .models import Movie
 # Create your views here.
 
 
 def show_all_movies(request):
-    movies = Movie.objects.all()
+    movies = Movie.objects.order_by('name')
+    agg = movies.aggregate(Avg('budjet'), Max('year'), Avg('rating'))
     return render(request, 'movie_app/all_movies.html', {
-        "movies": movies
+        "movies": movies,
+        "agg": agg
     })
 
 def show_one_movie(request, slug_movie):
