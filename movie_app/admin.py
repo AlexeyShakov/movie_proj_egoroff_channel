7b6d9a3b384db6a773bac32058a73f03d4a69a84
AlexeyShakov <alexei_96@inbox.ru>
2022-07-29 23:01:23 +0300

    created own filters, added searching line

--- movie_app/admin.py
+++ movie_app/admin.py
@@ -1,5 +1,30 @@
 from django.contrib import admin
 from .models import Movie
+from django.db.models import QuerySet
+
+class RatingFilter(admin.SimpleListFilter):
+
+    title = "Фильтр по рейтингу"
+    parameter_name = "rating"
+
+    def lookups(self, request, model_admin):
+        # The list of this values will be shown in the right side of the admin panel in the site
+        return [
+            ("<40", "Низкий"),
+            ("от 40 до 59", "Средний"),
+            ("от 60 до 84", "Высокий"),
+            (">= 85", "Высочайший"),
+                ]
+    def queryset(self, request, queryset: QuerySet):
+        if self.value() == "<40":
+            return queryset.filter(rating__lt=40)
+        elif self.value() == "от 40 до 59":
+            return queryset.filter(rating__gte=40).filter(rating__lt=59)
+        elif self.value() == "от 60 до 84":
+            return queryset.filter(rating__gte=60).filter(rating__lt=84)
+        else:
+            return queryset.filter(rating__gte=85)
+
 
 # the decorator is needed to register our database table in admin panel
 # for displaying additional columns in admin panel
@@ -13,6 +38,9 @@ class Movie_admin(admin.ModelAdmin):
     # ordering is needed for ordering our data
     ordering = ["-rating"]
     list_per_page = 3
+    actions = ["set_dollars", "set_rubles", "set_euros"]
+    search_fields = ["name"]
+    list_filter = ["name", "currency", RatingFilter]
 
     # This method is needed to add new column without creating them in database table
     # The decorator is needed for the column ability of being ordered
@@ -28,6 +56,17 @@ class Movie_admin(admin.ModelAdmin):
             return f"Фильм огонь!"
 
     # It's needed for creating new actions with data in admin panel
-    def set_currency(self):
-        pass
+    # Queryset is related with ORM
+    @admin.action(description="Установить валюту в доллар")
+    def set_dollars(self, request, queryset: QuerySet):
+        queryset.update(currency=Movie.USD)
 
+    @admin.action(description="Установить валюту в рубли")
+    def set_rubles(self, request, queryset: QuerySet):
+        queryset.update(currency=Movie.RUB)
+
+    @admin.action(description="Установить валюту в евро")
+    def set_euros(self, request, queryset: QuerySet):
+        # Returns the amount of rows updated
+        count_updated = queryset.update(currency=Movie.EUR)
+        self.message_user(request, message=f"Было обновлено {count_updated} записей")
