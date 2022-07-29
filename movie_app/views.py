18ca6d3cb4af863170316b3c347dd801e898ba8e
AlexeyShakov <alexei_96@inbox.ru>
2022-07-29 12:28:47 +0300

    set up debug toolbar and added the example of annotate usage

--- movie_app/views.py
+++ movie_app/views.py
@@ -1,11 +1,17 @@
 from django.shortcuts import render, get_object_or_404
-from django.db.models import F, Sum, Max, Min, Count, Avg
+from django.db.models import F, Sum, Max, Min, Count, Avg, Value
 from .models import Movie
 # Create your views here.
 
 
 def show_all_movies(request):
-    movies = Movie.objects.order_by('name')
+    # movies = Movie.objects.order_by('name')
+    # By using annotate we can create new columns by using SQL query
+    # Value is needed to add new column in the table in the SQL query(not by creating new column in the table)
+    movies = Movie.objects.annotate(new_field_bool=Value(True),
+                                    new_field_false_boll=Value(False),
+                                    new_budjet=F('budjet') + 100,
+                                    )
     agg = movies.aggregate(Avg('budjet'), Max('year'), Avg('rating'))
     return render(request, 'movie_app/all_movies.html', {
         "movies": movies,
