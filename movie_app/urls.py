91575e00a4c65ff2184493cfeeb5abb066d45874
AlexeyShakov <alexei_96@inbox.ru>
2022-08-04 13:50:10 +0300

    Implemented the logic of getting actors and directors witn class-based way, with ListView and DetailView in particular

--- movie_app/urls.py
+++ movie_app/urls.py
@@ -1,11 +1,17 @@
 from django.urls import path
 from . import views
+from .views import ActorsView, DetailActorView, DirectorsView, DetailDirectorView
 
 urlpatterns = [
     path('', views.show_all_movies),
     path('movie/<slug:slug_movie>', views.show_one_movie, name='movie_detail'),
-    path('directors', views.show_all_directors),
-    path('directors/<int:director_id>', views.show_one_director, name='director_detail'),
-    path('actors', views.show_all_actors),
-    path('actors/<int:actor_id>', views.show_one_actor, name="actor_detail"),
+    # path('directors', views.show_all_directors),
+    path('directors', views.DirectorsView.as_view()),
+    # path('directors/<int:director_id>', views.show_one_director, name='director_detail'),
+    path('directors/<int:pk>', views.DetailDirectorView.as_view(), name='director_detail'),
+    # path('actors', views.show_all_actors),
+    path('actors/', views.ActorsView.as_view()),
+    # path('actors/<int:actor_id>', views.show_one_actor, name="actor_detail"),
+    path('actors/<int:pk>', views.DetailActorView.as_view(), name="actor_detail"),
+
 ]
--- movie_app/views.py
+++ movie_app/views.py
@@ -1,8 +1,39 @@
 from django.shortcuts import render, get_object_or_404
 from django.db.models import F, Sum, Max, Min, Count, Avg, Value
 from .models import Movie, Director, Actor
+from django.views.generic import ListView, DetailView
+
 # Create your views here.
 
+class ActorsView(ListView):
+    template_name = "movie_app/all_actors.html"
+    model = Actor
+
+
+class DetailActorView(DetailView):
+    template_name = "movie_app/one_actor.html"
+    model = Actor
+    # To use DetailView we have to point the parameter with the special name 'pk'(primary key) in urls.py
+    # DetailView saves the needed data in 'context' variable with the name of the model but in lowercase. So we have to indicate this name in .html
+    # # But we can change that by setting the needed variable
+    # context_object_name = "our_name"
+
+
+class DirectorsView(ListView):
+    template_name = "movie_app/all_directors.html"
+    model = Director
+
+
+class DetailDirectorView(DetailView):
+    template_name = "movie_app/one_directror.html"
+    model = Director
+    # To use DetailView we have to point the parameter with the special name 'pk'(primary key) in urls.py
+    # DetailView saves the needed data in 'context' variable with the name of the model but in lowercase. So we have to indicate this name in .html
+    # # But we can change that by setting the needed variable
+    # context_object_name = "our_name"
+
+
+# Functional way!
 
 def show_all_movies(request):
     # movies = Movie.objects.order_by('name')
@@ -24,31 +55,34 @@ def show_one_movie(request, slug_movie):
     return render(request, 'movie_app/one_movie.html', {
         "movie": movie
     })
+#
+#
+# def show_all_directors(request):
+#     directors = Director.objects.all()
+#     return render(request, 'movie_app/all_directors.html', {
+#         "directors": directors
+#     })
+#
+#
+# def show_one_director(request, director_id: int):
+#     director = get_object_or_404(Director, id=director_id)
+#     return render(request, 'movie_app/one_directror.html', {
+#         "director": director
+#     })
 
 
-def show_all_directors(request):
-    directors = Director.objects.all()
-    return render(request, 'movie_app/all_directors.html', {
-        "directors": directors
-    })
+# def show_all_actors(request):
+#     actors = Actor.objects.all()
+#     return render(request, 'movie_app/all_actors.html', {
+#         "actors": actors
+#     })
 
 
-def show_one_director(request, director_id: int):
-    director = get_object_or_404(Director, id=director_id)
-    return render(request, 'movie_app/one_directror.html', {
-        "director": director
-    })
+# def show_one_actor(request, actor_id):
+#     actor = get_object_or_404(Actor, id=actor_id)
+#     return render(request, 'movie_app/one_actor.html', {
+#         "actor": actor
+#     })
 
 
-def show_all_actors(request):
-    actors = Actor.objects.all()
-    return render(request, 'movie_app/all_actors.html', {
-        "actors": actors
-    })
 
-
-def show_one_actor(request, actor_id):
-    actor = get_object_or_404(Actor, id=actor_id)
-    return render(request, 'movie_app/one_actor.html', {
-        "actor": actor
-    })
